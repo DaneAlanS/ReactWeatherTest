1d126fdf5114850b80fd2f66861bc8e8c0360c97
DaneAlanS <daneasimoneau@gmail.com>
2020-04-01 12:29:53 -0500

    updated to use hooks

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,65 +1,59 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
 
-export default class Weather extends Component {
-  constructor(props) {
-    super(props);
+function Weather() {
+  const [weatherDesc, setWeatherDesc] = useState("");
+  const [tempature, setTemp] = useState("");
+  const [feelsLike, setFeelsLike] = useState("");
+  const [tempMax, setMax] = useState("");
+  const [tempMin, setMin] = useState("");
+  const [windSpeed, setWindSpeed] = useState("");
 
-    this.state = {
-      weatherDescription: "",
-      tempature: "",
-      feelsLike: "",
-      tempMax: "",
-      tempMin: "",
-      windSpeed: 0
-    };
-  }
-
-  getWeatherItems() {
-    const api = "56574a65ae2215c8ae6e414e5401c948";
+  function getWeatherItems() {
+    const apikey = "56574a65ae2215c8ae6e414e5401c948";
+    function convertKelvin(weatherData) {
+      return Math.round((weatherData * 9) / 5 - 459.67);
+    }
     Axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?id=4276452&appid=${api}`
+      `https://api.openweathermap.org/data/2.5/weather?id=4276452&appid=${apikey}`
     ).then(response => {
       const description = response.data.weather[0].description;
-      const temp = Math.round((response.data.main.temp * 9) / 5 - 459.67);
-      const feels = Math.round(
-        (response.data.main.feels_like * 9) / 5 - 459.67
-      );
-      const dailyMax = Math.round(
-        (response.data.main.temp_max * 9) / 5 - 459.67
-      );
-      const dailyMin = Math.round(
-        (response.data.main.temp_min * 9) / 5 - 459.67
-      );
-      console.log(response.data.main);
-      this.setState({ weatherDescription: description });
-      this.setState({ tempature: temp });
-      this.setState({ feelsLike: feels });
-      this.setState({ tempMax: dailyMax });
-      this.setState({ tempMin: dailyMin });
+      const temp = convertKelvin(response.data.main.temp);
+      const feels = convertKelvin(response.data.main.feels_like);
+      const dailyMax = convertKelvin(response.data.main.temp_max);
+      const dailyMin = convertKelvin(response.data.main.temp_min);
+
+      setWeatherDesc(description);
+      setTemp(temp);
+      setFeelsLike(feels);
+      setMax(dailyMax);
+      setMin(dailyMin);
+
+      console.log(response.data);
     });
   }
 
-  componentDidMount() {
-    this.getWeatherItems();
-  }
+  // Similar to componentDidMount and componentDidUpdate
+  useEffect(() => {
+    getWeatherItems();
+  });
 
-  render() {
-    return (
-      <div className="content-wrapper">
-        <div className="left-side">
-          <h1>Current Weather</h1>
-          <div className="weather-wrapper">
-            <h2>{this.state.weatherDescription}</h2>
-          </div>
-        </div>
-        <div className="right-side">
-          <h2>Tempature: {this.state.tempature} F</h2>
-          <h2>Feels like: {this.state.feelsLike} F</h2>
-          <h2>High: {this.state.tempMax} F</h2>
-          <h2>Low: {this.state.tempMin} F</h2>
+  return (
+    <div className="content-wrapper">
+      <div className="left-side">
+        <h1>Norton, Kansas</h1>
+        <h1>{weatherDesc}</h1>
+        <div className="weather-wrapper">
+          <h2>Tempature: {tempature} F</h2>
         </div>
       </div>
-    );
-  }
+      <div className="right-side">
+        <h2>Feels like: {feelsLike} F</h2>
+        <h2>High: {tempMax} F</h2>
+        <h2>Low: {tempMin} F</h2>
+      </div>
+    </div>
+  );
 }
+
+export default Weather;
